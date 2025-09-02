523b0ec60f60dc52cfc235eeecf3f04f28cdb331
joshua <le24uben@studserv.uni-leipzig.de>
2025-09-02 13:39:51 +0200

    map now displays a polyline fetched from the backend.

--- frontend/app/components/MapComponent.tsx
+++ frontend/app/components/MapComponent.tsx
@@ -1,24 +1,47 @@
 "use client";
 
 import React, { useEffect, useRef } from "react";
-import { Map, MapStyle, config } from "@maptiler/sdk";
+import { Map, MapStyle, config, helpers } from "@maptiler/sdk";
 import "../map/mapstyle.css"; // importing my "mapstyle.css" for styling
 
 config.apiKey = "jgADwIPnUzhtC93OwbQm" // API for MapTiler FIXME: temporary
 
 const MapComponent: React.FC = () => {
     const mapContainer = useRef<HTMLDivElement>(null);
-
+    const mapRef = useRef<Map | null>(null);
     useEffect(() => {
-        if (mapContainer.current) {
-            const map = new Map({
+        if (mapContainer.current && !mapRef.current) {
+            mapRef.current = new Map({
                 container: mapContainer.current,
                 style: MapStyle.OUTDOOR,
                 center: [11.3662, 50.6493], // Breitengrad, Längengrad 
                 zoom: 10,
-            });
+            });   
         }
+
+        //just for testing
+        mapRef.current?.on("load", async () => {
+            try {
+                const response = await fetch("http://localhost:8080/route/routeGeoJson?id=1");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const routeGeoJson = await response.json();
+
+                await helpers.addPolyline(mapRef.current!, {
+                    data: routeGeoJson,
+                    outline: true,
+                    outlineWidth: 3,
+                    lineWidth: 3,
+                    outlineColor: "#ff0000",
+                })
+            } catch (error) {
+                console.error("Error fetching route GeoJSON:", error);
+            }
+        });
     }, []);
+
+    
 
     return <div id="map" ref={mapContainer} />;
 };
--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,7 +1,11 @@
+import MapComponent from "../components/MapComponent";
+
 export default function Home() {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
+            <p>This is my current map</p>
+            <MapComponent />
         </div>
     )
 }
