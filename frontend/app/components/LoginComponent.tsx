b70d430e936123f718dd4e0a48bb44c06cd84c53
boschmitj <joshua.kretschmer1@gmail.com>
2025-10-27 15:29:00 +0100

    some changes on the styling

--- frontend/app/components/LoginComponent.tsx
+++ frontend/app/components/LoginComponent.tsx
@@ -1,40 +1,48 @@
 import React, { useState } from "react";
 import LoginInputComponent from "./LoginInputComponent";
 import "./LoginComponent.css"
+import Button from "./Button";
 
 const LoginComponent : React.FC = () => {
 
-    const [isSignUp, setIsSignUp] = useState(true)
+    const [isSignUp, setIsSignUp] = useState(false)
 
     return (
         <div className="login-container centered">
-            <button onClick={() => setIsSignUp(false)}>Sign In</button>
-            <button onClick={() => setIsSignUp(true)}>Sign Up</button>
-            <p>{isSignUp ? "Sign up" : "Sign In"} here</p>
-            { isSignUp ? (
-                    <form action={sendSignUp} method="post" id="signup-form">
-                        <LoginInputComponent id="nickname" type="text" required text="Nickname" />
-                        <LoginInputComponent id="email" type="email" required text="Email" />
-                        <LoginInputComponent id="password" type="password" required text="Password" />
-                    </form>
-                ) : 
-                (
-                    <form action={sendSignIn} method="post" id="signin-form">
-                        <LoginInputComponent id="email" type="email" required text="Email" />
-                        <LoginInputComponent id="password" type="password" required text="Password" />
-                    </form>
-                )
-            }
+            <div className="heading__login">
+                <h2>{isSignUp ? "Sign up" : "Sign in"} here</h2>
+            </div>
+            <div className="toggle-buttons__login">
+                <Button
+                    variant={!isSignUp ? "primary" : "secondary"}
+                    onClick={() => setIsSignUp(false)}
+                >
+                    Sign In
+                </Button>
+                <Button
+                    variant={isSignUp ? "primary" : "secondary"}
+                    onClick={() => setIsSignUp(true)}
+                >
+                    Sign Up
+                </Button>
+            </div>
+            
+            
+            <form onSubmit={handleSubmit} className="login-form">
+                {isSignUp && (
+                    <LoginInputComponent id="nickname" type="text" required text="Nickname" />
+                )}
+                <LoginInputComponent id="email" type="email" required text="Email" />
+                <LoginInputComponent id="password" type="password" required text="Password" />   
+
+                <Button type="submit">{isSignUp ? "Sign Up" : "Sign In"}</Button> 
+            </form>
         </div>
     )
 }
 
-const sendSignUp = () => {
-
-}
-
-const sendSignIn = () => {
-
+const handleSubmit = async () => {
+    
 }
 
 
