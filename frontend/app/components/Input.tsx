64868e28826824761b3f3df3a3af305798a076ae
boschmitj <joshua.kretschmer1@gmail.com>
2025-11-19 18:48:01 +0100

    sending of route now works, but the ui is bugged. Also normal login via signup doesnt work. User isnt added to the db

--- frontend/app/components/Input.tsx
+++ frontend/app/components/Input.tsx
@@ -23,6 +23,39 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 
+// const Input: React.FC<InputProps> = ({ value, onChange, placeholder, type = "text", dataTitle, onConfirm }) => {
+//   return (
+//     <>
+//       <input className="c-checkbox" type="checkbox" id="checkbox" />
+//       <div className="c-formContainer">
+//         <div className="c-form">
+//           <input
+//             className="c-form__input"
+//             value={value}
+//             onChange={onChange}
+//             placeholder={placeholder}
+//             type={type}
+//             required
+//           />
+//           <label className="c-form__buttonLabel" htmlFor="checkbox">
+//             <button className="c-form__button" type="button" onClick={e => {
+//               e.preventDefault();
+//               e.stopPropagation();
+//               onConfirm();
+//             }}>
+//               Send
+//             </button>
+//           </label>
+//           <label className="c-form__toggle" htmlFor="checkbox" data-title={dataTitle}></label>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+
+
+// this version works, but the ui is bugged now
 const Input: React.FC<InputProps> = ({ value, onChange, placeholder, type = "text", dataTitle, onConfirm }) => {
   return (
     <>
@@ -37,19 +70,21 @@ const Input: React.FC<InputProps> = ({ value, onChange, placeholder, type = "tex
             type={type}
             required
           />
-          <label className="c-form__buttonLabel" htmlFor="checkbox">
-            <button className="c-form__button" type="button" onClick={e => {
+          <button 
+            className="c-form__button" 
+            type="button" 
+            onClick={(e) => {
+              e.preventDefault();
               e.stopPropagation();
               onConfirm();
-            }}>
-              Send
-            </button>
-          </label>
+            }}
+          >
+            Send
+          </button>
           <label className="c-form__toggle" htmlFor="checkbox" data-title={dataTitle}></label>
         </div>
       </div>
     </>
   );
 };
-
 export default Input;
