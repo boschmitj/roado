4947e68a2b440f03d43c6da4ac2cb77087b1acc2
joshua <joshua.kretschmer1@gmail.com>
2025-12-23 13:18:41 +0100

    Feat: Add TimePositionObject and TimeSpeedObject entities

---
+++ backend/demo/src/main/java/com/roado/demo/Model/TimeSpeedObject.java
@@ -0,0 +1,21 @@
+package com.roado.demo.Model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TimeSpeedObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long time;
+    private Double speed;
+}
