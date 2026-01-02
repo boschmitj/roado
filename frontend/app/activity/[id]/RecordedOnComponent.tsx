ef3f97a39c87ee709d2089439b13588afc572864
joshua <joshua.kretschmer1@gmail.com>
2026-01-02 16:02:48 +0100

    Feat: Format time display in RecordedOnComponent to ensure consistent two-digit minute representation

--- frontend/app/activity/[id]/RecordedOnComponent.tsx
+++ frontend/app/activity/[id]/RecordedOnComponent.tsx
@@ -8,8 +8,8 @@ export default function RecordedOnComponent({startedAt, endedAt} : RecordedOnCom
     console.log("Ended at: " + endedAt);
     const startDate = new Date(startedAt);
     const endDate = new Date(endedAt);
-    const startTime = `${startDate.getHours()}:${startDate.getMinutes()}`;
-    const endTime = `${endDate.getHours()}:${endDate.getMinutes()}`;
+    const startTime = `${startDate.getHours()}:${startDate.getMinutes().toString().padStart(2, '0')}`;
+    const endTime = `${endDate.getHours()}:${endDate.getMinutes().toString().padStart(2, '0')}`;
     return (
         <div className="text-sm text-zinc-600">
             Recorded on {startTime} - {endTime}
